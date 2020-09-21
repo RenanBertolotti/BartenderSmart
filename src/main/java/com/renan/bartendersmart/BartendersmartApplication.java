761a49fe562bc30c07eeeb0e071973307ad7bacb
RenanBertolotti <renan.bertolotti@gmail.com>
2020-09-21 17:50:49 -0300

    Projeto Criado

---
+++ src/main/java/com/renan/bartendersmart/BartendersmartApplication.java
@@ -0,0 +1,13 @@
+package com.renan.bartendersmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BartendersmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BartendersmartApplication.class, args);
+	}
+
+}
