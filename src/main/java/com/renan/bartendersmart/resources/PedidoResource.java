dc1cb362f41e6a246e5fb0dcb67f104558e9ac1e
Renan Bertolotti <42254677+RenanBertolotti@users.noreply.github.com>
2020-11-02 10:02:21 -0300

    Update PedidoResource.java

--- src/main/java/com/renan/bartendersmart/resources/PedidoResource.java
+++ src/main/java/com/renan/bartendersmart/resources/PedidoResource.java
@@ -15,32 +15,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.renan.bartendersmart.entities.Pedido;
-import com.renan.bartendersmart.services.PedidoService;
+import com.renan.bartendersmart.entities.Produto;
+import com.renan.bartendersmart.services.ProdutoService;
 
 @RestController
-@RequestMapping(value = "/pedidos")
-public class PedidoResource {
+@RequestMapping(value = "/produtos")
+public class ProdutoResource {
 	
 	@Autowired
-	private PedidoService service;
+	private ProdutoService service;
 
 	@GetMapping
-	public ResponseEntity<List<Pedido>> findAll(){		
-		List<Pedido> list = service.findAll();
+	public ResponseEntity<List<Produto>> findAll(){		
+		List<Produto> list = service.findAll();
 		
 		return ResponseEntity.ok().body(list);
 	}
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Pedido> findById(@PathVariable Long id){
-		Pedido obj = service.findById(id);
+	public ResponseEntity<Produto> findById(@PathVariable Long id){
+		Produto obj = service.findById(id);
 		
 		return ResponseEntity.ok().body(obj);
 	}
-
+	
 	@PostMapping
-	public ResponseEntity<Pedido> insert(@RequestBody Pedido obj){
+	public ResponseEntity<Produto> insert(@RequestBody Produto obj){
 		obj = service.insert(obj);
 		
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
@@ -56,7 +56,7 @@ public class PedidoResource {
 	}
 	
 	@PutMapping(value = "/{id}")
-	public ResponseEntity<Pedido> update(@PathVariable Long id, @RequestBody Pedido obj){
+	public ResponseEntity<Produto> update(@PathVariable Long id, @RequestBody Produto obj){
 		obj = service.update(id, obj);
 		
 		return ResponseEntity.ok().body(obj);		
