f43e2d6f10255be86f7f8e1cd3ccc671769d8a18
Renan Bertolotti <42254677+RenanBertolotti@users.noreply.github.com>
2020-11-02 10:08:15 -0300

    Update ProdutoService.java

--- src/main/java/com/renan/bartendersmart/services/ProdutoService.java
+++ src/main/java/com/renan/bartendersmart/services/ProdutoService.java
@@ -1,30 +1,64 @@
-package com.renan.bartendersmart.services;
+package com.renan.bartendersmart.resources;
 
+import java.net.URI;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.renan.bartendersmart.entities.Produto;
-import com.renan.bartendersmart.repositories.ProdutoRepository;
+import com.renan.bartendersmart.entities.Mesa;
+import com.renan.bartendersmart.services.MesaService;
 
-@Service
-public class ProdutoService {
+@RestController
+@RequestMapping(value = "/mesas")
+public class MesaResource {
 	
 	@Autowired
-	private ProdutoRepository repository;
-	
+	private MesaService service;
+
 	@GetMapping
-	public List<Produto> findAll(){
-		return repository.findAll();
+	public ResponseEntity<List<Mesa>> findAll(){		
+		List<Mesa> list = service.findAll();
+		
+		return ResponseEntity.ok().body(list);
 	}
 	
-	@GetMapping
-	public Produto findById(Long id) {
-		Optional<Produto> obj = repository.findById(id);
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Mesa> findById(@PathVariable Long id){
+		Mesa obj = service.findById(id);
 		
-		return obj.get();
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Mesa> insert(@RequestBody Mesa obj){
+		obj = service.insert(obj);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Mesa> update(@PathVariable Long id, @RequestBody Mesa obj){
+		obj = service.update(id, obj);
+		
+		return ResponseEntity.ok().body(obj);		
 	}
 }
