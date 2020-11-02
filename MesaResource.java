1639b3a9f51f1cbe9096113bac765eed2ecac60d
Renan Bertolotti <42254677+RenanBertolotti@users.noreply.github.com>
2020-11-02 09:57:05 -0300

    Add files via upload

---
+++ MesaResource.java
@@ -0,0 +1,64 @@
+package com.renan.bartendersmart.resources;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.renan.bartendersmart.entities.Mesa;
+import com.renan.bartendersmart.services.MesaService;
+
+@RestController
+@RequestMapping(value = "/mesas")
+public class MesaResource {
+	
+	@Autowired
+	private MesaService service;
+
+	@GetMapping
+	public ResponseEntity<List<Mesa>> findAll(){		
+		List<Mesa> list = service.findAll();
+		
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Mesa> findById(@PathVariable Long id){
+		Mesa obj = service.findById(id);
+		
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Mesa> insert(@RequestBody Mesa obj){
+		obj = service.insert(obj);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Mesa> update(@PathVariable Long id, @RequestBody Mesa obj){
+		obj = service.update(id, obj);
+		
+		return ResponseEntity.ok().body(obj);		
+	}
+}
